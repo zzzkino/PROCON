63dabf35dfcf92249119e5efe4b7e8060a782680
yoshida_home <toshiki@yoshidatoshiki-no-MacBook-Air.local>
2014-11-24 00:45:54 +0900

    test中

--- benkyou3.py
+++ benkyou3.py
@@ -1,9 +1,41 @@
-charnum={'A':1, 'B':2, 'C':3, 'D':4, 'E':5,
-         'F':6, 'G':7, 'H':8, 'I':9, 'J':10,
-         'K':11, 'L':12, 'M':13, 'N':14, 'O':15,
-         'P':16, 'Q':17, 'R':18, 'S':19, 'T':20,
-         'U':21, 'V':22, 'W':23, 'X':24, 'Y':25, 'Z':26}
+dic={'A':1, 'B':2, 'C':3, 'D':4, 'E':5,
+    'F':6, 'G':7, 'H':8, 'I':9, 'J':10,
+    'K':11, 'L':12, 'M':13, 'N':14, 'O':15,
+    'P':16, 'Q':17, 'R':18, 'S':19, 'T':20,
+    'U':21, 'V':22, 'W':23, 'X':24, 'Y':25, 'Z':26}
 
 
+def charT(N,S):
+    i = 0
+    T = ''
+    while i < N-1:
+        if dic[S[0]] > dic[S[-1]]:
+            T = T + S[-1]
+            print(T)
+            S = S[:-1]
+            i += 1
+        elif dic[S[0]] < dic[S[-1]]:
+            T = T + S[0]
+            print(T)
+            S = S[1:]
+            i += 1
+        else:
+            i += 1
+            SS = S
+            while dic[SS[0]] == dic[SS[-1]]:
+                SS = SS[1:-1]
+                if len(SS) <= 2:
+                    break
+            if dic[SS[0]] > dic[SS[-1]]:
+                T = T + S[-1]
+                S = S[:-1]
+                print(T)
+            else:
+                T = T + S[0]
+                S = S[1:]
+                print(T)
 
 
+N = 7
+S = 'HKHAHLH'
+charT(N,S)
