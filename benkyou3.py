1469ae53ae3824d8760649421f17b12baa125cb7
yoshida_home <toshiki@yoshidatoshiki-no-MacBook-Air.local>
2014-11-25 22:00:21 +0900

    printを消した

--- benkyou3.py
+++ benkyou3.py
@@ -4,41 +4,37 @@ dic={'A':1, 'B':2, 'C':3, 'D':4, 'E':5,
     'P':16, 'Q':17, 'R':18, 'S':19, 'T':20,
     'U':21, 'V':22, 'W':23, 'X':24, 'Y':25, 'Z':26}
 
-
 def charT(N,S):
     i = 0
     T = ''
     while i < N:
         if dic[S[0]] > dic[S[-1]]:
             T = T + S[-1]
-            print(T)
             S = S[:-1]
             i += 1
         elif dic[S[0]] < dic[S[-1]]:
             T = T + S[0]
-            print(T)
             S = S[1:]
             i += 1
         else:
             i += 1
             SS = S
-            if len(SS) > 2:
+            if len(SS) > 3:
                 while dic[SS[0]] == dic[SS[-1]]:
                     SS = SS[1:-1]
-                    if len(SS) <= 2:
+                    if len(SS) < 3:
                         break
                 if dic[SS[0]] > dic[SS[-1]]:
                     T = T + S[-1]
                     S = S[:-1]
-                    print(T)
                 else:
                     T = T + S[0]
                     S = S[1:]
-                    print(T)
             else:
                 T = T + S[0]
-                print(T)
+                S = S[1:]
+    print(T)
 
-N = 7
-S = 'ABFFFCA'
+N = 8
+S = 'ABFCAFCA'
 charT(N,S)
