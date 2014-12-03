25f4588a8159dfdd17f792130968e5b117450bdf
yoshida_home <toshiki@yoshidatoshiki-no-MacBook-Air.local>
2014-12-03 22:38:40 +0900

    first commit

---
+++ procon5.py
@@ -0,0 +1,17 @@
+
+def counter(N,L):
+    counter = 0
+    for i in range(N-1):
+        L.sort()
+        X = L.pop(0)
+        Y = L.pop(0)
+        counter += X+Y
+        L.append(X+Y)
+        print(L)
+        print(counter)
+
+
+
+N = 3
+L = [8,5,8]
+counter(N,L)
