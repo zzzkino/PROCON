f874f6a500ba60adf87500b2a0c639d2a8693442
yoshida_home <toshiki@T-Mac.local>
2015-01-08 00:21:29 +0900

    no message

--- procon6/gen_zip.py
+++ procon6/gen_zip.py
@@ -3,28 +3,42 @@ text=File.read()
 File.close()
 print(text)
 chardict={}
-
 for char in text:
     if char in chardict:
         chardict[char]=chardict[char]+1
     else:
         chardict[char]=1
-print(chardict)
 CharCountTuple=chardict.items()
 SortedCharCountTuple=sorted(CharCountTuple, key=lambda x:x[1],reverse=True)
-print(SortedCharCountTuple)
 SortedCharList=[]
 for tuple in SortedCharCountTuple:
     SortedCharList.append(tuple[0])
-print(SortedCharList)
 n=len(SortedCharList)
 list_01=[]
 for i in range(0,n):
     code_01=''
     code_01=code_01+'1'*i+'0'
     list_01.append(code_01)
-print(list_01)
 CharCodeDict={}
+CodeCharDict={}
 for i in range(0,n):
     CharCodeDict[SortedCharList[i]]=list_01[i]
+    CodeCharDict[list_01[i]]=SortedCharList[i]
 print(CharCodeDict)
+print(CodeCharDict)
+text_zip=''
+for char in text:
+    text_zip=text_zip+CharCodeDict[char]
+print(text_zip)
+
+newtext=''
+while len(text_zip) !=0:
+    A='0'
+    while text_zip[0]=='1':
+        text_zip.pop(0)
+        A='1'+A
+    newtext= newtext + CodeCharDict[A]
+print(newtext)
+
+
+
