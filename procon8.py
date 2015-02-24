1c6a4af877fabcbe9ee3a0aa430f68a935f677d6
yoshida <yoshidatoshiki@yoshida-no-MacBook-Air.local>
2015-02-24 09:26:27 +0900

    間違え発見。

--- procon8.py
+++ procon8.py
@@ -22,20 +22,34 @@ def make_listoflist(char1, char2):
     return large_list
 
 
-def countsubsequence(listoflist):
-    counter = 0
-    memo = -1
+def find_maxnum_and_cahnge_listoflist(listoflist):
+    max_list = []
+    newlistoflist = []
     for i in range(len(listoflist)):
-        for num in listoflist[i]:
-            if memo < num:
-                memo = num
-                counter +=1
-                break
-        print(memo)
-    print('答えは'+str(counter))
+        if listoflist[i] != []:
+            max_list.append(max(listoflist[i]))
+            newlistoflist.append(listoflist[i])
+    listoflist = newlistoflist
+    return [max(max_list), listoflist]
+
+
+def solve(listoflist):
+    max_num = find_maxnum_and_cahnge_listoflist(listoflist)[0]
+    listoflist = find_maxnum_and_cahnge_listoflist(listoflist)[1]
+    box = [(-1,0)]
+    for i in range(max_num+1):
+        box.append((i))
+    for list in listoflist:
+        for tuple in box:
+            if len(tuple) == 2:
+                for el in list:
+                    if tuple[0] < el:
+                        box.append(el, tuple[1]+1)
+                    
+
 
 
 s = 'aiyd'
 t = 'aiudadf'
 print(make_listoflist(s, t))
-countsubsequence(make_listoflist(s, t))
+find_maxnum(make_listoflist(s, t))
